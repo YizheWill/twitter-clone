2a5a3fe1f79cbb5efcb274ddbf683163519aac60
Yizhe Wang <yizhe87@gmail.com>
2020-12-23 21:05:48 -0800

    added jwt and bcrypt part

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import User from '../../models/User.js';
+import { secretOrKey } from '../../config/keys.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 const router = Router();
+
+// register new user
 
 router.post('/register', (req, res) => {
   User.findOne({ email: req.body.email }).then((user) => {
@@ -19,11 +23,37 @@ router.post('/register', (req, res) => {
           newUser.password = hashedPassword;
           newUser
             .save()
-            .then((userInfo) => res.json(userInfo))
+            .then((userInfo) => {
+              const payload = { id: userInfo.id, handle: userInfo.handle };
+              jwt.sign(payload, secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                res.send({ success: true, token: 'Bearer ' + token });
+              });
+            })
             .catch((err) => console.log('err', err));
         });
       });
     }
+  });
+});
+
+// login a user
+
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email }).then((user) => {
+    if (!user) {
+      res.status(404).json({ user: 'no such a user' });
+    }
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        const payload = { id: user.id, handle: user.handle };
+        jwt.sign(payload, secretOrKey, { expiresIn: 3600 }, (err, token) => {
+          res.json({ success: true, token: 'Bearer ' + token });
+        });
+      } else {
+        res.status(400).json({ password: 'wrong pass' });
+      }
+    });
   });
 });
 
