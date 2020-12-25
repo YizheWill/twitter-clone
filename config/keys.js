23db8b99bd6194f4a9f7817b68b3766d616b7b67
Yizhe Wang <yizhe87@gmail.com>
2020-12-24 19:27:49 -0800

    change keys

--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,13 @@
+import { mongoURI as prodM, secretOrKey as prodS } from './keys_prod.js';
 let m;
 let s;
 
 if (process.env.NODE_ENV === 'production') {
-  import { mongoURI as prodM, secretOrKey as prodS } from './keys_prod.js';
   m = prodM;
   s = prodS;
 } else {
-  import { mongoURI as devM, secretOrKey as devS } from './keys_dev.js';
-  m = devM;
-  s = devS;
+  m = '';
+  s = '';
 }
 
 export const mongoURI = m;
